52c40d873ce18cc7087f7db3c965c636bef34e7c
Joseph Zhang <josephzhang@lucky2019s-mbp.lan>
2020-08-20 18:47:45 -0400

    Documentation

--- instabot.py
+++ instabot.py
@@ -24,11 +24,15 @@ sleep(2)
 password_input = driver.find_element_by_name("password")
 password_input.send_keys(password)
 sleep(2)
+# Login
 driver.find_element_by_xpath("//button[@type='submit']").click()
 sleep(3)
+# Ignore the request to save information
 driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button").click()
 sleep(2)
+# Clicks not now for notifications
 driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
 sleep(3)
+# Clicks the Explore Page Link
 driver.find_element_by_xpath("//a[@href='/explore/']").click()
 sleep(4)
